cf8b07151aa09fb8bd7fcd82961cb359685a6847
Peter <petersolomon704@gmail.com>
2023-04-21 08:11:47 +0100

    Update AddBlog.jsx
    Modified code

--- src/components/blog/AddBlog.jsx
+++ src/components/blog/AddBlog.jsx
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
+import { addBlog } from "../../services/blog.service";
 
 const AddBlog = () => {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newBlogObject = {
+        title,
+        author,
+        url,
+      };
+
+      const response = await addBlog(newBlogObject);
+      if (response) {
+        setSuccessMessage("Blog successfully added");
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 5000);
+      }
+    } catch (error) {
+      setErrorMessage(error);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
   return (
     <div className="add-blog-wrapper">
       <h2 className="create-blog-title">Creat A Blog</h2>
-      <form className="form-wrap">
+      <form className="form-wrap" onSubmit={handleSubmit}>
         <input
           className="add-blog-input"
           type="text"
